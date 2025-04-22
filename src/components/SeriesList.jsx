e9996e20fbc2f717470f8908a57c2e792a004c4a
Nicolas Duarte Silva <nickboytube0418@gmail.com>
2025-04-22 11:55:26 -0300

    ...

--- src/components/SeriesList.jsx
+++ src/components/SeriesList.jsx
@@ -24,7 +24,6 @@ export default function SeriesList() {
 
     useEffect(() => {
         loadSeries();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page]);
 
     const handleOpenModal = (serie) => {
